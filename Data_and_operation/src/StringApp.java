593f2bfd5cf638eb6e6a6aa246244b843436eb0b
iamminho <minho100227@kakao.com>
2022-08-02 19:39:11 +0900

    6.4 문자열의 표현

---
+++ Data_and_operation/src/StringApp.java
@@ -0,0 +1,16 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // String
+		System.out.println('a'); // Character
+		System.out.println("H");
+		
+//		new line
+		System.out.println("Hello\nWorld");
+		
+//		escape
+		System.out.println("\"/");
+	}
+
+}
