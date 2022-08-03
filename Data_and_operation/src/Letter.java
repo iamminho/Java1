cccf8d5b5e2a36aa3167dade8ba681712ea0dd37
iamminho <minho100227@kakao.com>
2022-08-03 18:42:04 +0900

    8.3 변수의 효용

---
+++ Data_and_operation/src/Letter.java
@@ -0,0 +1,13 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "leezche";
+		String name2 = "leezche2";
+		System.out.println("Hello, "+name+" ... egoing ... "+name2+" ... egoing ... egoing ... bye");
+		
+		
+
+	}
+
+}
